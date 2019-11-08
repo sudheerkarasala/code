fb542f306ef932ff9dfec326efb726ebc170a157
sudheer <sudheer.karasala9@gmail.com>
2019-11-08 14:18:25 +0530

    first commit

--- payvoo-web-PV-590/sources/Web/src/app/home/personal/accounts/exchange/event-emitter.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable,EventEmitter } from '@angular/core';
-@Injectable({
-  providedIn: 'root'
-})
-export class EventEmitterService {
-
- public myEvent=new EventEmitter
-  constructor() { }
-}
--- payvoo-web-PV-590/sources/services/router/dashboardStatus.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import {getDashboardStatus, postDashboardStatus, patchDashboardStatus} from '../controller/dashboardStatus';
-
-router.patch('/service/status', patchDashboardStatus);
-router.post('/service/statusInsert', postDashboardStatus);
-router.post('/service/status', getDashboardStatus);
-
-module.exports = router;
